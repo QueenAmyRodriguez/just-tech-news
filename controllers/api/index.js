6faf24b50ebe2ac31b7487f5cf0c16d750f57621
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-03-16 13:33:01 -0700

    added edit and delete functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const dashboardRoutes = require('../dashboard-routes.js');
 
 
 router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
